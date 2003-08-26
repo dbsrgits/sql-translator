5210e290c7ab4227532344a05dddf8c44e4c1bc9
Ken Youens-Clark <kclark@cpan.org>
2003-08-26 16:26:33 +0000

    Alias to XML::SQLFairy.

---
+++ lib/SQL/Translator/Parser/XML.pm
@@ -0,0 +1,54 @@
+package SQL::Translator::Parser::XML;
+
+# -------------------------------------------------------------------
+# $Id: XML.pm,v 1.1 2003-08-26 16:26:33 kycl4rk Exp $
+# -------------------------------------------------------------------
+# Copyright (C) 2003 Ken Y. Clark <kclark@cpan.org>.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; version 2.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# 02111-1307  USA
+# -------------------------------------------------------------------
+
+=pod
+
+=head1 NAME
+
+SQL::Translator::Parser::XML - Alias to XML::SQLFairy parser
+
+=head1 DESCRIPTION
+
+This module is an alias to the XML::SQLFairy parser.
+
+=head1 SEE ALSO
+
+SQL::Translator::Parser::XML::SQLFairy.
+
+=head1 AUTHOR
+
+Ken Y. Clark E<lt>kclark@cpan.orgE<gt>.
+
+=cut
+
+# -------------------------------------------------------------------
+
+use strict;
+use vars qw[ $VERSION $DEBUG ];
+$VERSION = sprintf "%d.%02d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/;
+$DEBUG = 1 unless defined $DEBUG;
+
+use SQL::Translator::Parser::XML::SQLFairy;
+
+*parse = \&SQL::Translator::Parser::XML::SQLFairy::parse;
+
+1;
