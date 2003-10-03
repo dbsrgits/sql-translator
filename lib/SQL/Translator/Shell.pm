70e2a181570ea249379305c83c3a2ede1f2354a9
Darren Chamberlain <dlc@sevenroot.org>
2003-10-03 21:13:37 +0000

    SQL::Translator::Shell - and interactive Schema browser.  This is a placeholder commit.

---
+++ lib/SQL/Translator/Shell.pm
@@ -0,0 +1,18 @@
+package SQL::Translator::Shell;
+
+# ----------------------------------------------------------------------
+# $Id: Shell.pm,v 1.1 2003-10-03 21:13:37 dlc Exp $ 
+# ----------------------------------------------------------------------
+# SQL::Translator::Shell - interactive interface to a parsed schema
+# Copyright (C) 2003 darren chamberlain <darren@cpan.org>
+# ----------------------------------------------------------------------
+
+use strict;
+use vars qw($VERSION $REVISION $DEBUG);
+
+
+1;
+
+__END__
+
+# This is a placeholder!
