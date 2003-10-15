dd6fb3de515be1a6cad88cd6412fccc70e486b7a
Ken Youens-Clark <kclark@cpan.org>
2003-10-15 16:48:39 +0000

    Added boilerplate intro/copyright.

--- lib/SQL/Translator/Producer/Turnkey.pm
+++ lib/SQL/Translator/Producer/Turnkey.pm
@@ -1,8 +1,29 @@
 package SQL::Translator::Producer::Turnkey;
 
+# -------------------------------------------------------------------
+# $Id: Turnkey.pm,v 1.9 2003-10-15 16:48:39 kycl4rk Exp $
+# -------------------------------------------------------------------
+# Copyright (C) 2003 Allen Day <allenday@ucla.edu>,
+#   Brian O'Connor <brian.oconnor@excite.com>.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; version 2.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# 02111-1307  USA
+# -------------------------------------------------------------------
+
 use strict;
 use vars qw[ $VERSION $DEBUG ];
-$VERSION = sprintf "%d.%02d", q$Revision: 1.8 $ =~ /(\d+)\.(\d+)/;
+$VERSION = sprintf "%d.%02d", q$Revision: 1.9 $ =~ /(\d+)\.(\d+)/;
 $DEBUG   = 1 unless defined $DEBUG;
 
 use SQL::Translator::Schema::Constants;
