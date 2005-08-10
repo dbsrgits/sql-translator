afb0748302af33e2a92b175492a5b76b78bc1523
Chris Hilton <chilton@thoughtworks.com>
2005-08-10 16:44:17 +0000

    Modified equals() to include case insensitive data type checking and removed is_unique check (better caught by a constraint check)

--- lib/SQL/Translator/Schema/Field.pm
+++ lib/SQL/Translator/Schema/Field.pm
@@ -1,7 +1,7 @@
 package SQL::Translator::Schema::Field;
 
 # ----------------------------------------------------------------------
-# $Id: Field.pm,v 1.25 2005-07-18 20:16:02 duality72 Exp $
+# $Id: Field.pm,v 1.26 2005-08-10 16:44:17 duality72 Exp $
 # ----------------------------------------------------------------------
 # Copyright (C) 2002-4 SQLFairy Authors
 #
@@ -50,7 +50,7 @@ use base 'SQL::Translator::Schema::Object';
 
 use vars qw($VERSION $TABLE_COUNT $VIEW_COUNT);
 
-$VERSION = sprintf "%d.%02d", q$Revision: 1.25 $ =~ /(\d+)\.(\d+)/;
+$VERSION = sprintf "%d.%02d", q$Revision: 1.26 $ =~ /(\d+)\.(\d+)/;
 
 # Stringify to our name, being careful not to pass any args through so we don't
 # accidentally set it to undef. We also have to tweak bool so the object is
@@ -567,13 +567,12 @@ Determines if this field is the same as another
     
     return 0 unless $self->SUPER::equals($other);
     return 0 unless $case_insensitive ? uc($self->name) eq uc($other->name) : $self->name eq $other->name;
-    #return 0 unless $self->is_valid eq $other->is_valid;
-    return 0 unless $self->data_type eq $other->data_type;
+    return 0 unless lc($self->data_type) eq lc($other->data_type);
     return 0 unless $self->size eq $other->size;
     return 0 unless defined $self->default_value eq defined $other->default_value;
     return 0 if defined $self->default_value && $self->default_value ne $other->default_value;
     return 0 unless $self->is_nullable eq $other->is_nullable;
-    return 0 unless $self->is_unique eq $other->is_unique;
+#    return 0 unless $self->is_unique eq $other->is_unique;
     return 0 unless $self->is_primary_key eq $other->is_primary_key;
 #    return 0 unless $self->is_foreign_key eq $other->is_foreign_key;
     return 0 unless $self->is_auto_increment eq $other->is_auto_increment;
