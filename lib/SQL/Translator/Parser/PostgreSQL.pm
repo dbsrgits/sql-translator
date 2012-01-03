88ad825597d4eee0bf3c93aa81738f82cc583fae
Cedric Carree <cedric.carree@mancalanetworks.com>
2012-01-03 17:53:56 +0100

    SQLT::Parser::PostgreSQL parses table def with default values
    Actually the parser used to skip table definition which includes fields with explicitely casted default values
    For example, the following field definition was not working (due to the "::text" part)
    
    CREATE TABLE foo (
        bar TEXT DEFAULT 'this is casted default text'::TEXT
    );

--- lib/SQL/Translator/Parser/PostgreSQL.pm
+++ lib/SQL/Translator/Parser/PostgreSQL.pm
@@ -936,7 +936,7 @@ create_table : CREATE TABLE
 
 create_index : CREATE /index/i
 
-default_val  : DEFAULT /(\d+|'[^']*'|\w+\(.*\))|\w+/
+default_val  : DEFAULT /(\d+|'[^']*'|\w+\(.*\))|\w+/ ( '::' data_type )(?)
     {
         my $val =  defined $item[2] ? $item[2] : '';
         $val    =~ s/^'|'$//g;
