dbsrgits/sql-translator

commit 0c04c5a2210135419771878dc7e341a1cba52cca

our > use vars

lib/SQL/Translator/Producer/Latex.pm
@@ -37,8 +37,8 @@ automatically arrange them horizontally or vertically.
 use strict;
 use warnings;
 
-use vars qw[ $DEBUG $VERSION @EXPORT_OK ];
-$VERSION = '1.59';
+our @EXPORT_OK;
+our $VERSION = '1.59';
 $DEBUG   = 0 unless defined $DEBUG;
 
 use SQL::Translator::Utils 'debug';

commit 53d5396046cc601d477f014eef2dcfd21a47a735

Fix silly syntax error, introduced in 0c04c5a22

lib/SQL/Translator/Producer/Latex.pm
@@ -39,7 +39,6 @@ use warnings;
 
 our @EXPORT_OK;
 our $VERSION = '1.59';
-$DEBUG   = 0 unless defined $DEBUG;
 
 use SQL::Translator::Utils 'debug';
 