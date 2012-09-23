f56361ad7eea3245af7ec5b0eaed5ca439bd6168
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-09-23 10:15:42 -0500

    release v0.11013_02

--- lib/SQL/Translator.pm
+++ lib/SQL/Translator.pm
@@ -3,7 +3,7 @@ package SQL::Translator;
 use Moo 1.000003;
 our ( $DEFAULT_SUB, $DEBUG, $ERROR );
 
-our $VERSION  = '0.11013_01';
+our $VERSION  = '0.11013_02';
 $DEBUG    = 0 unless defined $DEBUG;
 $ERROR    = "";
 
