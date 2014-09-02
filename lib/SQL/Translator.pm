5ae23b58609a3a30567bedd4f9f1c04d824be42f
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2014-09-02 15:13:48 +0100

    release 0.11019

--- lib/SQL/Translator.pm
+++ lib/SQL/Translator.pm
@@ -3,7 +3,7 @@ package SQL::Translator;
 use Moo;
 our ( $DEFAULT_SUB, $DEBUG, $ERROR );
 
-our $VERSION  = '0.11018';
+our $VERSION  = '0.11019';
 $DEBUG    = 0 unless defined $DEBUG;
 $ERROR    = "";
 
