22a15de6fc368a505623cf5eb08e9ea3f8c99eba
Ben Vallerand <vallerand.ben@gmail.com>
2022-08-24 10:35:18 -0400

    Make sure the sqlt_args actually get passed in to the SQL::Translator::Diff object. Can be an issue if SQL::Translator::Diff::schema_diff is called directly.

--- lib/SQL/Translator/Diff.pm
+++ lib/SQL/Translator/Diff.pm
@@ -92,7 +92,7 @@ sub schema_diff {
     $options ||= {};
 
     my $obj = SQL::Translator::Diff->new( {
-      %$options,
+      sqlt_args => $options,
       source_schema => $source_schema,
       target_schema => $target_schema,
       output_db     => $output_db
